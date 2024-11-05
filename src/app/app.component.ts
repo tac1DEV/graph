9982585e582b8e7c87abeb15a189279255a2f97e
tac1DEV <sansberrot@gmail.com>
2024-11-05 18:49:57 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Chart, registerables } from 'chart.js';
+import { Chart, Legend, plugins, registerables } from 'chart.js';
 
 Chart.register(...registerables);
 @Component({
@@ -12,64 +12,64 @@ Chart.register(...registerables);
 })
 export class AppComponent implements OnInit{
   public config: any = {
-    type: 'line',
+    type: 'bar',
     data: {
-      labels:[
-        "7 Nov", "8 Nov", "9 Nov", "10 Nov", "11 Nov", "12 Nov", "13 Nov",
-        "14 Nov","15 Nov", "16 Nov", "17 Nov","18 Nov", "19 Nov", "20 Nov",
-        "21 Nov", "22 Nov", "23 Nov", "24 Nov", "25 Nov", "26 Nov", "27 Nov",
-        "28 Nov","29 Nov", "30 Nov", "1 Dec", "2 Dec", "3 Dec", "4 Dec", "5 Dec"
-    ]
-    ,
-      datasets: [
-        {
-          label: 'past 28 days',
-          data: [
-            5450, 5250, 4260, 2450, 2800, 5400, 5250,
-            5050, 4500, 2250, 2500, 5480, 5600, 4250,
-            3900, 2250, 2500, 5250, 5400, 5250, 5100,
-            4250, 2500, 2450, 2425, 2400, 4950, 5050
-          ],
-          fill: false,
-          borderWidth: 4,
-          borderColor: 'rgb(6,209,255)',
-          backgroundColor: 'rgb(6,209,255)',
-          tension: 0.1
-        },
-        {
-          label: 'prev 28 days',
-          data: [
-            5350, 5000, 4260, 2200, 2800, 5550, 5250,
-            5250, 4600, 2250, 2400, 5480, 5400, 4150,
-            3850, 2300, 2550, 5200, 5300, 5250, 5200,
-            4400, 2400, 2350, 2425, 2400, 5500, 5350
-          ],
-          fill: false,
-          borderWidth: 4,
-          borderColor: 'rgb(228,200,82)',
-          backgroundColor: 'rgb(228,200,82)',
-          tension: 0.1
-        },
-      ]
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      datasets: [{
+        label: 'Weekly Sales',
+        data: [1],
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+        ],
+        borderWidth: 1,
+        borderSkipped: false,
+        borderRadius: 5,
+        barPercentage: 0.2,
+        categoryPercentage: 0.9,
+      }]
     },
-    options: {
-      maintainAspectRatio: false,
-      scales:{
-        x:{
-          grid:{
-            color: "transparent"
-          }
-        },
-        y:{
-          grid:{
-            color: "#404268"
-          }
-        }
+  options: {
+    indexAxis: 'y',
+    plugins: {
+      legend: {
+        display: false
       },
     },
+    maintainAspectRatio: false,
+    scales:{
+      x:{
+        grid:{
+          display: false,
+        },
+        border:{
+          display: false
+        },
+        ticks:{
+          color: "white"
+        }
+      },
+      y:{
+        beginAtZero: true,
+        grid:{
+          display: false,
+          drawBorder: false
+        },
+        border:{
+          display: false
+        },
+        ticks:{
+          display: false,
+        }
+      }
+    },
+  },
   };
   chart: any;
   ngOnInit():void{
     this.chart = new Chart('MyChart', this.config);
   }
 }
+
